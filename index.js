0c007eef02761b2baf50087431effb4b616347ea
wentingzoe <ywtzoe@gmail.com>
2021-12-02 19:52:12 -0500

    Fetch Geo Coordinates By IP

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('162.245.144.188',(error, coordinates)=>{
+  if (error) {
+    console.log("GEO data didn't work!" , error);
+    return;
+  }
+  console.log('GEO worked! Returned coordinates:' , coordinates);
 });
--- iss.js
+++ iss.js
@@ -2,24 +2,40 @@ const request = require('request');
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   const url = 'https://api.ipify.org?format=json';
-  request.get(url, function(err, response, body) {
+  request.get(url, (err, response, body)=> {
     if (err) {
-      console.log("Here was a problem: ", err)
-      callback(null, err);
-      return;
-    }
+      console.log("Here was a problem: ", err);
+      return callback(error, null);
+    };
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(error(msg), null);
+      callback(Error(msg), null);
       return;
-    }
-    const ip = JSON.parse(body);
-    if (ip.length === 0) {
-      callback(err, null);
-    } else {
-      callback(null,ip.ip);
-    }
+    };
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
 };
+////////////////////////////////////////////
+const fetchCoordsByIP = function(ip, callback){
+  // error can be set if invalid domain, user is offline, etc.
+  request.get(`https://freegeoip.app/json/${ip}`, (err, response, body) =>{
+  if (err) {
+    callback(error, null);
+    return;
+  }
+  // if non-200 status, assume server error
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+ 
+  // if we get here, all's well and we got the data
+  const { latitude, longitude } = JSON.parse(body);
 
-module.exports = { fetchMyIP };
+  callback(null, { latitude, longitude });
+});
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP};
